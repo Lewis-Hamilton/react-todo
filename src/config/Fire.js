c4c2d3e8773e496d5f0d7487951822315a54a02c
unknown <hamiltonjlewis@gmail.com>
2019-08-26 21:54:05 -0500

    broken

---
+++ src/config/Fire.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAhYCWZNOSLFgwlOOwIlFti9maE-wix8ak",
+  authDomain: "react-todo-28f64.firebaseapp.com",
+  databaseURL: "https://react-todo-28f64.firebaseio.com",
+  projectId: "react-todo-28f64",
+  storageBucket: "",
+  messagingSenderId: "675220266727",
+  appId: "1:675220266727:web:a636ecd3392c562a"
+};
--- src/index.js
+++ src/index.js
@@ -2,8 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import firebase from 'firebase';
+import { Login } from './pages/Login';
+import { Fire } from './config/Fire';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+firebase.auth().onAuthStateChanged(user => {
+  if (user) {
+    ReactDOM.render(<App user={user} />, document.getElementById('root'));
+  } else {
+    ReactDOM.render(<Login />, document.getElementById('root'));
+  }
+});
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
